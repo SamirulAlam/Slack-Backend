f0deb0a48e81f8810fe8a916b07bcd30265f5058
Samirul Alam <samirulalam372@gmail.com>
2021-03-02 05:25:11 +0600

    made it realtime

--- server.js
+++ server.js
@@ -30,6 +30,21 @@ mongoose.connect(connection_url,{
 })
 mongoose.connection.once("open",()=>{
     console.log("DB Connected")
+
+    const changeStream =mongoose.connection.collection("conversations").watch();
+    changeStream.on("change",(change)=>{
+        if(change.operationType==="insert"){
+            pusher.trigger("channels","newChannel",{
+                "change": change
+            })
+        }else if(change.operationType==="update"){
+            pusher.trigger("conversation","newMessage",{
+                "change": change
+            })
+        }else{
+            console.log("Error")
+        }
+    })
 })
 //api routes
 app.get("/",(req, res) =>{
